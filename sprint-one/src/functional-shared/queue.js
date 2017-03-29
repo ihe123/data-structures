d9951ecea3acfe5b894796deef94394a008ce83d
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:59:25 -0700

    update

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var result = {'storage':  {},
+	'count': 0 };
+  _.extend(result, queueMethods);
+
+  return result;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	
+	'size' : function(){
+		return this.count;
+	},
+	'enqueue': function(info){
+		this.storage[this['count']] = info;
+		this.count++;
+		
+	},
+	'dequeue': function(){
+		if (this.count > 0){
+		var first = this.storage[0];
+		var sec = this.storage[1];
+		delete first;
+		this.storage[0] = sec;
+		this.count--;
+		
+	}
+	return first;
+	}
+}
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var result = {};
+  _.extend(result, stackMethods);
+
+  return result;
 };
 
-var stackMethods = {};
-
+var stackMethods = {
+	'count': 0 , 
+	'size' : function(){
+		return this.count;
+	},
+	'push': function(info){
+		this[this.count] = info;
+		this.count++;
+		
+	},
+	'pop': function(){
+		if (this.count > 0){
+		var popped = this[this.count];
+		delete popped;
+		this.count--;
+		return this[this.count];
+	}
+	
+	}
+};
 
