ebaf3379c7d61946a0654a1bfc383cd5052e71c6
Ivana <ivana.he.lili@gmail.com>
2017-03-30 10:17:29 -0700

    "refactor code"

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -2,34 +2,39 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var obj = Object.create(queueMethods);
-	 obj.count = 0;
-	 obj.storage = {};
+	 // obj.count = 0;
+	 // obj.storage = {};
 	return obj
 
 };
 
 var queueMethods = {
+	'count' : 0, 
 	'size' : function(){
 		return this.count;
 	},
 	'enqueue': function(info){
-		this.storage[this.count] = info;
+		this[this.count] = info;
 		this.count++;
 		
 	},
 	'dequeue': function(){
 	if (this.count > 0){
-		var first = this.storage[0];
+		var first = this[0];
 		// var sec = this.storage[1];
 		delete first;
 		// this.storage[0] = sec;
-
-		for (var key in this.storage){
-			this.storage[key] = this.storage[key+1];
+		for (var i = 0; i<this.count; i++){
+			this[i] = this [i+1];
 		}
+		// for (var key in this){
+		// 	console.log(key);
+		// 	this[key] = this[key+1];
+		// }
 		this.count--;
+		return first;
 	}
-	return first;
+	
 	}
 		// if (this.count > 0){
 		// var popped = this[this.count];
