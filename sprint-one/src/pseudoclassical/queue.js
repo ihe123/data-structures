75dd9307f07314575b74611f744ddc89952dba1b
Ivana <ivana.he.lili@gmail.com>
2017-03-30 10:18:02 -0700

    "working on format"

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
+    // this = Object.create(Queue.prototype);
+    // return this 
+}
+    Queue.prototype.count = 0;
+    Queue.prototype.push = function (val){
+        // obj[count] = val; 
+        // count++;
+    }
+    Queue.prototype.pop = function (){
+        // var popped = obj[0];
+        // delete obj[0];
+        // // for (var i = 0; ){
+
+        // // }
+        // count--;
+        // return popped;
+    }
+    Queue.prototype.size = function (){
+        return this.count;
+    }
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+
 
 
