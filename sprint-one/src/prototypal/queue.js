9d532ccd131df27fe336e969a0114b7f6ce0fb15
Hack Reactor Student <student@hackreactor.com>
2017-03-29 19:51:55 -0700

    update

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+	 obj.count = 0;
+	 obj.storage = {};
+	return obj
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+	'size' : function(){
+		return this.count;
+	},
+	'enqueue': function(info){
+		this.storage[this.count] = info;
+		this.count++;
+		
+	},
+	'dequeue': function(){
+	if (this.count > 0){
+		var first = this.storage[0];
+		var sec = this.storage[1];
+		delete first;
+		this.storage[0] = sec;
+		this.count--;
+		
+	}
+	return first;
+	}
+		// if (this.count > 0){
+		// var popped = this[this.count];
+		// delete popped;
+		// this.count--;
+		// return this[this.count];
+		// }
+	}
+
 
 
