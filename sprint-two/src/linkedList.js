3287f99e8008834dba0afc9ff6f0f6dc76ee4286
Hack Reactor Student <student@hackreactor.com>
2017-03-30 19:52:57 -0700

    completed linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -7,32 +7,37 @@ var LinkedList = function() {
     var node = new Node(value); //new node is created with the value that is passed in
     if(list.tail !== null){ // if list.tail exists, enter the if block
       var currentTail = list.tail; //store that tail into currentTail
-      currentTail.next = node; //
+      currentTail.next = node; //the node that the currentTail is pointing to is now the new node
     }
-    list.tail = node;
-    if(list.head === null){
-        list.head = node;
+    list.tail = node; //assign the current tail to the new node
+    if(list.head === null){ // if list is empty
+        list.head = node; //assign the list.head to the new node
     }
     
   };
 
   list.removeHead = function() {
-    var oldHead = list.head.value; 
-    var newHead = list.head.next;
-      list.head = newHead;
-      return oldHead;
+    var oldHead = list.head.value; //the value of list.head is stored in variable called oldHead
+    var newHead = list.head.next; //the node following the list.head is assigned to variable called newHead
+      list.head = newHead; //assign list.head to the newHead
+      return oldHead; //return the oldHead
   };
 
   list.contains = function(target) {
-    var currentNode = list.head;
-    while(currentNode.next !== null){
+    var currentNode = list.head; //set current head to variable currentnode
+
+    while(currentNode.next !== null){ //if the 
 
       if (currentNode.value === target){
         return true;
       } else {
         currentNode = currentNode.next;
       }
+
     }
+     if (currentNode.value === target){
+        return true;
+      } 
     
     return false;
   };
