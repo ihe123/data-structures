a0c4483c0e00848e48e96fbb8e92f9818dc73041
Hack Reactor Student <student@hackreactor.com>
2017-04-01 14:57:19 -0700

    complete binaryTree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -14,7 +14,7 @@ describe('binarySearchTree', function() {
   it('should insert values at the correct location in the tree', function() {
     debugger;
 
-    binarySearchTree.insert(5);    
+    binarySearchTree.insert(2);    
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
     binarySearchTree.insert(6);
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,59 +1,50 @@
 var BinarySearchTree = function(value) {
-	var tree = binaryTree(value);
+	var tree = Object.create(treePrototype);
+	tree.value = value;
+	tree.left = null;
+	tree.right = null;
+	return tree;
+}
 
-	tree.left = function(){
-		return tree.l;
+var treePrototype= {};
 
-	};
-
-	tree.right = function(){
-		return tree.r;
-	};
-
-	tree.insert = function(value){
-		var root =BinarySearchTree(value);
-		var thing = Object.assign(root, thing)
-		// console.log(root);
-		// var obj = Object.assign(root,  );
-		// console.log(root);
-		// console.log(tree)
-		// var t = tree.value;
-		if (tree.value > value){
-			
-			if (tree.l === null && tree.r === null){
-
-				thing.insert(value);
-				console.log(thing);
-
-				
-			} else {
-				root = Object.assign(root, tree.insert(value));
-				// console.log(root);
-				// compare = tree.l;
-				tree.l = binaryTree(value);
-				tree.l.insert(tree.l.value);
-				
-			}
+treePrototype.insert = function(value){
+	if (value < this.value){
+		if (this.left === null){
+			this.left = BinarySearchTree(value);
 		} else {
-			if (this.l === null && this.r === null){
-				this.r = binaryTree(value);
-			} else {
-				// tree = tree.r;
-				// value = tree.r.value;
-				this.r.insert(value);
+			this.left.insert(value);	
+		}
+	} else if (value > this.value) {
+		if (this.right === null){
+			this.right = BinarySearchTree(value)
+		} else {
+			this.right.insert(value);
+		}
+	}
+};
+
+	treePrototype.contains = function(value){
+		if (value === this.value){
+			return true;
+		} else if(value < this.value){
+			return !!(this.left && this.left.contains(value));
+		} else if(value > this.value){
+			return !!(this.right && this.right.contains(value));
+		}
+
+	};
+
+	treePrototype.depthFirstLog = function(func){
+		func(this.value);
+			if (this.left){
+				this.left.depthFirstLog(func);
+			}
+			if (this.right){
+				this.right.depthFirstLog(func);
 			}
 		}
-		
 	
-
-	};
-
-	tree.contains = function(){
-
-	};
-
-	tree.depthFirstLog = function(func){
-		
 		// Left, Root, Right
 		//  if tree.l exists.. tree.l becomes the new tree
 	
@@ -63,19 +54,15 @@ var BinarySearchTree = function(value) {
 		//  else take root ;
 		// 
 		// after ^ check if tree.r exists and invoke above functions again			
-	};
-	return tree;
 
-};
+// var binaryTree = function(value) {
+//   var node = {};
 
-var binaryTree = function(value) {
-  var node = {};
-
-  node.value = value;
-  node.l = null;
-  node.r = null;
-  return node;
-};
+//   node.value = value;
+//   node.l = null;
+//   node.r = null;
+//   return node;
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
